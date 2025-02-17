40a15537b01d5316439e1ae30271d4e8be057564
Liana <145667748+liana1402@users.noreply.github.com>
2025-02-17 11:47:08 +0000

    make models for candidate, staff, and admin including the member variables and functions

---
+++ NEXTGroup3/NEXTGroup3/Models/Candidate.cs
@@ -0,0 +1,16 @@
+﻿namespace NEXTGroup3.Models
+{
+    public class Candidate : User
+    {
+        //EDIT CANDIDATE ACCOUNT
+        public void EditAccount()
+        {
+
+        }
+        //DELETE CANDIDATE ACCOUNT
+        public void DeleteAccount()
+        {
+
+        }
+    }
+}
---
+++ NEXTGroup3/NEXTGroup3/Models/Staff.cs
@@ -0,0 +1,13 @@
+﻿namespace NEXTGroup3.Models
+{
+    public class Staff : User
+    {
+        // STAFF ID
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+}
