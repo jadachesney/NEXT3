f1b7ad3a0233869b5b4ebc14591e62b5105f8c16
Liana <145667748+liana1402@users.noreply.github.com>
2025-02-17 15:11:41 +0000

    created text question and text answer models

---
+++ NEXTGroup3/NEXTGroup3/Models/TextQuestion.cs
@@ -0,0 +1,13 @@
+﻿namespace NEXTGroup3.Models
+{
+    public class TextQuestion : Question
+    {
+        //ANSWERS
+        private List<TextAnswer> answers;
+        public List<TextAnswer> Answers
+        {
+            get { return answers; }
+            set { answers = value; }
+        }
+    }
+}
