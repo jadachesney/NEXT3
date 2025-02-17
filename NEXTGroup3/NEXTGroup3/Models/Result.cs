f05af5ac79035ad7d0ef9843694f0cd931a5084a
gigo890 <149149857+gigo890@users.noreply.github.com>
2025-02-17 14:58:02 +0000

    Result Model added

---
+++ NEXTGroup3/NEXTGroup3/Models/Result.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Permissions;
+
+namespace NEXTGroup3.Models
+{
+    public class Result
+    {
+        //-----ATTRIBUTES-----//
+
+        //---RESULT ID---//
+        private int id;
+        public int Id { get { return id; } set { id = value; } }
+
+        //---DEPARTMENT---//
+        private Department department;
+        public Department Department { get { return department; } set { department = value; } }
+
+        //---DEPARTMENT POINTS---//
+        public enum departmentPoints
+        {
+            IT,
+            Finance,
+            Marketing,
+            Design,
+            Trading,
+            Merchandising
+        }
+        public class Foo
+        {
+            public departmentPoints DepartmentPoints { get; set; }
+        }
+
+
+        //---ROLES---//
+        public enum roles { };
+        public class Fooo
+        {
+            public roles Roles { get; set; }
+        }
+    }
+}
