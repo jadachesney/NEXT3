b48d77c0c273f3446856c763956c593db319fd23
jadachesney <148222075+jadachesney@users.noreply.github.com>
2025-02-17 14:19:46 +0000

    Connected to Azure database, reworked the scaffolding for dep and role, removed roles CRUD, added migrations.

--- NEXTGroup3/NEXTGroup3/Data/ApplicationDbContextFactory.cs
+++ NEXTGroup3/NEXTGroup3/Data/ApplicationDbContextFactory.cs
@@ -1,16 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore.Design;
+//using Microsoft.EntityFrameworkCore;
 
-namespace NEXTGroup3.Data
-{
-    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<NEXTGroup3Context>
-    {
-        public NEXTGroup3Context CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<NEXTGroup3Context>();
-            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EcommerceDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+//namespace NEXTGroup3.Data
+//{
+//  public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<NEXTGroup3Context>
+//  {
+//    public NEXTGroup3Context CreateDbContext(string[] args)
+//    {
+//      var optionsBuilder = new DbContextOptionsBuilder<NEXTGroup3Context>();
+//      optionsBuilder.UseSqlServer("Server=tcp:nextgroup3server.database.windows.net,1433;Initial Catalog=NEXTGroup3;Persist Security Info=False;User ID=JadaAdmin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
-            return new NEXTGroup3Context(optionsBuilder.Options);
-        }
-    }
-}
+//      return new NEXTGroup3Context(optionsBuilder.Options);
+//    }
+//  }
+//}
--- NEXTGroup3/NEXTGroup3/Program.cs
+++ NEXTGroup3/NEXTGroup3/Program.cs
@@ -1,15 +1,37 @@
 ﻿using NEXTGroup3.Client.Pages;
 using NEXTGroup3.Components;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NEXTGroup3.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<AzureContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureContext") ?? throw new InvalidOperationException("Connection string 'AzureContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+var connection = String.Empty;
+if (builder.Environment.IsDevelopment())
+{
+  builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+  connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+}
+else
+{
+  connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+}
+
+//builder.Services.AddDbContext<DbContextNext>(options =>
+//    options.UseSqlServer(connection));
+
 var app = builder.Build();
+
+var logConnectionString = builder.Configuration.GetConnectionString("AzureContext");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -33,5 +55,7 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(NEXTGroup3.Client._Imports).Assembly);
+
+
 
 app.Run();
