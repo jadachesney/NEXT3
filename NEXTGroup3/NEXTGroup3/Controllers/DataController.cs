2ccefd8bc364ca67e9209fc71a26f4f7dc81aa50
gigo890 <149149857+gigo890@users.noreply.github.com>
2025-02-19 18:38:42 +0000

    Added TextAnswerService to DataController

---
+++ NEXTGroup3/NEXTGroup3/Controllers/DataController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NEXTGroup3.Data;
+using NEXTGroup3.Models;
+
+namespace NEXTGroup3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private readonly AzureContext context;
+
+        public DataController(AzureContext c)
+        {
+            this.context = c;
+        }
+
+        // GET: api/data
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RangeQuestion>>> GetAllRangeQuestions()
+        {
+            var data = await context.RangeQuestion.AsNoTracking().ToListAsync();
+            return Ok(data);
+        }
+        public async Task<ActionResult<IEnumerable<TextAnswer>>> GetAllTextAnswers()
+        {
+            var data = await context.TextAnswer.AsNoTracking().ToListAsync();
+            return Ok(data);
+        }
+    }
+}
