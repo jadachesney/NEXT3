fc784a473c7ccaa936d693e0f451fb5f4217b06c
gigo890 <149149857+gigo890@users.noreply.github.com>
2025-02-19 18:36:38 +0000

    TextAnswer Service added

---
+++ NEXTGroup3/NEXTGroup3/Services/TextAnswerService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NEXTGroup3.Data;
+using NEXTGroup3.Models;
+
+namespace NEXTGroup3.Services
+{
+    public class TextAnswerService
+    {
+        private readonly AzureContext context;
+
+        public TextAnswerService(AzureContext c)
+        {
+            context = c;
+        }
+
+        public async Task<List<TextAnswer>> GetAllTextAnswers()
+        {
+            return await context.TextAnswer.AsNoTracking().ToListAsync();
+        }
+    }
+}
