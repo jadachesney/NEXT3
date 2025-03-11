d08a6b4ab4fa22754998d20b87d8b5ab80c7f7f2
Liana <145667748+liana1402@users.noreply.github.com>
2025-03-11 18:49:02 +0000

    Successfully implemented points system on range based questions. A list of all the departments with their points is obtained at the end of the range questions section.

--- NEXTGroup3/NEXTGroup3/Program.cs
+++ NEXTGroup3/NEXTGroup3/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using NEXTGroup3.Data;
 using NEXTGroup3.Controllers;
 using NEXTGroup3.Services;
+using NEXTGroup3.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContextFactory<AzureContext>(options =>
@@ -17,6 +18,8 @@ builder.Services.AddRazorComponents()
 builder.Services.AddScoped<TextAnswerService>();
 builder.Services.AddScoped<RoleService>();
 builder.Services.AddScoped<RangeQuestionService>();
+builder.Services.AddScoped<ResultService>();
+builder.Services.AddScoped<DepartmentService>();
 
 
 builder.Services.AddQuickGridEntityFrameworkAdapter();
---
+++ NEXTGroup3/NEXTGroup3/Services/ResultService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NEXTGroup3.Data;
+using NEXTGroup3.Models;
+
+namespace NEXTGroup3.Services
+{
+    public class ResultService
+    {
+        private readonly AzureContext context;
+        public Result Result { get; set; }
+
+        public ResultService(AzureContext c)
+        {
+            context = c;
+            Result = new Result();
+        }
+
+        public async Task<List<DepartmentRangeQuestion>> GetAllDepartmentRangeQuestions()
+        {
+            return await context.DepartmentRangeQuestion.AsNoTracking().ToListAsync();
+        }
+    }
+}
