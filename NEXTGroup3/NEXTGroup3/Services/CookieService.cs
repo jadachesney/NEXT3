bc06f43b884529799cc4d239622fa14798a9c73d
jadachesney <148222075+jadachesney@users.noreply.github.com>
2025-03-27 14:19:11 +0000

    Cookie consent banner completed, styling might have to be changed later on depending on final dev push

---
+++ NEXTGroup3/NEXTGroup3/Services/CookieService.cs
@@ -0,0 +1,20 @@
+﻿using NEXTGroup3.Data;
+using NEXTGroup3.Models;
+
+namespace NEXTGroup3.Services
+{
+  public class CookieService
+  {
+    private readonly AzureContext context;
+    public bool CookieConsented { get; set; } = false;
+
+    public CookieService(AzureContext c)
+    {
+      context = c;
+    }
+    public void ConsentCookie()
+    {
+      CookieConsented = true;
+    }
+  }
+}
